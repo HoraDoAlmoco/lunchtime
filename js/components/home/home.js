2e0f25f3c24637357410eb4fefb920e3ee271249
hsalmeida <hsalmeida@gmail.com>
2016-02-01 12:10:14 -0200

    home e login

--- js/components/home/home.js
+++ js/components/home/home.js
@@ -3,4 +3,50 @@ lunchtime.controller('HomeController', ['$scope', '$rootScope', '$state', '$cook
         $rootScope.bodybg = {
             background: '#db4437 url(img/core/cover2.png) no-repeat fixed center'
         };
+
+        var angularCookieUsrObj = $cookies.getObject('lunchtimeCookieKeyUsrObj');
+        if(angularCookieUsrObj) {
+            $rootScope.currentUser = angularCookieUsrObj;
+        }
+
+        if($rootScope.currentUser && $rootScope.currentUser.remember) {
+            $scope._email = $rootScope.currentUser.email;
+            $scope._remem = $rootScope.currentUser.remember;
+        }
+
+        function assignCurrentUser (user) {
+            $rootScope.currentUser = user;
+            $cookies.put("grupoPrincipalKey", user.grupoPrincipal.id);
+            return user;
+        }
+
+        $scope.submit = function(email, password, rememberMe) {
+            var query = {
+                "email" : email,
+                "password" : password
+            };
+            User.query(query).then(function(user){
+                $scope.notFound = false;
+                if(user[0]){
+                    var curUser = user[0];
+
+                    curUser.remember = rememberMe;
+                    assignCurrentUser(curUser);
+                    if(rememberMe) {
+                        $cookies.putObject('angularCookieKeyUsrObj', curUser);
+                    }
+                    var appQuery = {
+                        "nome": user.grupoPrincipal
+                    }
+                    Grupo.query(appQuery).then(function(grupos){
+                        if(grupos[0]){
+                            $cookies.putObject("grupoPrincipal", grupos[0]);
+                        }
+                    });
+                    $state.go('welcome');
+                } else {
+                    $scope.notFound = true;
+                }
+            });
+        };
     }]);
--- js/lunchtime.js
+++ js/lunchtime.js
@@ -3,6 +3,22 @@ var lunchtime = angular.module('lunchtime', [
     'ui.router','ngResource', 'mongolabResourceHttp',
     'ngCookies', 'ngAnimate'
     ])
+    .run(function ($rootScope, $state, $cookies) {
+
+        $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+
+            var requireLogin = toState.data.requiredlogin;
+            //ver se esta no cookie
+            var angularCookieUsrObj = $cookies.getObject('angularCookieKeyUsrObj');
+            if(angularCookieUsrObj) {
+                $rootScope.currentUser = angularCookieUsrObj;
+            }
+            if (requireLogin && typeof $rootScope.currentUser === 'undefined') {
+                event.preventDefault();
+                $state.go('home');
+            }
+        });
+    })
     .constant('MONGOLAB_CONFIG',{API_KEY:'YXgR-q92vuVCKlSm-ji3nplDTE7rHIQh', DB_NAME:'ltdb'})
     .config(function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise("/");
