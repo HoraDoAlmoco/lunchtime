bb77fcf8983ccef3a6cd6d9572e5f02eeceff583
hsalmeida <hsalmeida@gmail.com>
2016-02-01 11:39:55 -0200

    home e login

--- js/components/home/home.js
+++ js/components/home/home.js
@@ -1,4 +1,6 @@
 lunchtime.controller('HomeController', ['$scope', '$rootScope', '$state', '$cookies',
     function($scope, $rootScope, $state, $cookies){
-        $scope.bodybg = "loginbg";
+        $rootScope.bodybg = {
+            background: '#db4437 url(img/core/cover2.png) no-repeat fixed center'
+        };
     }]);
