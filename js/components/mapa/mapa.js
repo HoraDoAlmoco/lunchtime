28d509ffb7c731681168464f1846afdcc69ded2e
hsalmeida <hsalmeida@gmail.com>
2016-02-03 17:23:48 -0200

    inicio do mapa

--- js/components/mapa/mapa.js
+++ js/components/mapa/mapa.js
@@ -33,16 +33,13 @@ lunchtime.controller('MapaController', ['$scope', '$rootScope', '$state', '$stat
         $scope.map.mapTypes.set('lunchtimemap', lunchtimeMapType);
         $scope.map.setMapTypeId('lunchtimemap');
 
-        var primarker = new google.maps.Marker({
+        var primarker = new MarkerWithLabel({
             map: $scope.map,
             position: new google.maps.LatLng(grupoPrincipal.latitude, grupoPrincipal.longitude),
-            title: grupoPrincipal.nome,
+            labelContent: grupoPrincipal.nome,
+            labelClass: "marker-labels",
+            labelAnchor: new google.maps.Point((grupoPrincipal.nome.length * 3), 0),
             icon : "img/core/localprincipal.png"
-        });
-
-        google.maps.event.addListener(primarker, 'click', function(){
-            $scope.infowindow.setContent("<span>Grupo: " + grupoPrincipal.nome + "</span>");
-            $scope.infowindow.open($scope.map, primarker);
         });
 
         $scope.markers = [];
@@ -62,7 +59,70 @@ lunchtime.controller('MapaController', ['$scope', '$rootScope', '$state', '$stat
                 }
             };
             Locais.query(query).then(function(locais){
-                console.log(locais);
+                var groupLatLng = new google.maps.LatLng(grupoPrincipal.latitude, grupoPrincipal.longitude);
+                locais.forEach(function(local){
+                    console.log(local);
+                    var info = {};
+                    var localLatLng = new google.maps.LatLng(local.latitude, local.longitude);
+                    var service = new google.maps.DistanceMatrixService();
+                    var distancia;
+                    service.getDistanceMatrix(
+                        {
+                            origins: [groupLatLng],
+                            destinations: [localLatLng],
+                            travelMode: google.maps.TravelMode.WALKING,
+                            unitSystem: google.maps.UnitSystem.METRIC,
+                            avoidHighways: false,
+                            avoidTolls: false,
+                        }, function(response, status){
+                            distancia = response.rows[0].elements[0].distance.value;
+                        });
+
+                    info.titulo = local.grupos[0].titulo;
+                    info.latitude = local.latitude;
+                    info.longitude = local.longitude;
+                    info.votos = local.grupos[0].votos;
+                    info.votado = local.grupos[0].votado;
+
+                    createMarker(info);
+                });
             });
         };
+
+        var createMarker = function(info) {
+            var eixox = 1;
+            var eixoy = 136;
+            if(Number(info.votos[0]) > 1){
+                eixox = (Number(info.votos[0]) - 1) * 35 + 1;
+            }
+            if(info.votado != "") {
+                eixoy = 92;
+            }
+
+            var icon;
+
+            if(Number(info.votos[0]) > 0){
+                icon = new google.maps.MarkerImage("/lunchtime/img/core/mappin-sprite.png", new google.maps.Size(33, 42), new google.maps.Point(eixox, eixoy));
+            } else {
+                icon = new google.maps.MarkerImage("/lunchtime/img/core/mappin-blank.png", new google.maps.Size(33, 42));
+            }
+
+            var halfPoint = Math.ceil(info.titulo.length * 3);
+            var marker = new MarkerWithLabel({
+                map: $scope.map,
+                position: new google.maps.LatLng(info.latitude, info.longitude),
+                icon : icon,
+                animation : google.maps.Animation.DROP,
+                labelContent: info.titulo,
+                labelClass: "marker-labels",
+                labelAnchor: new google.maps.Point(halfPoint, 0)
+            });
+
+            google.maps.event.addListener(marker, 'click', function(){
+                console.log("abrir a telinha em baixo");
+            });
+
+            $scope.markers.push(marker);
+        };
+
     }]);
