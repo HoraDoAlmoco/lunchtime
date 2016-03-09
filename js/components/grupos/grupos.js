7973d4b2f772ee84c609bf14556e7a9a8e51ca44
hsalmeida <hsalmeida@gmail.com>
2016-03-09 17:00:45 -0300

    atualizacao geral de codigo

--- js/components/grupos/grupos.js
+++ js/components/grupos/grupos.js
@@ -60,6 +60,23 @@ angular.module('lunchtime').controller('GroupController', ['$scope', '$rootScope
 
         }
 
+        $scope.criarGrupo = function () {
+            $modal
+                .open({
+                    templateUrl: 'addgrupo.html',
+                    controller: 'AddGrupoController',
+                    resolve: {
+                        ctrlScope: function () {
+                            return $scope
+                        }
+                    }
+                }).result.then(function () {
+                    $state.reload();
+                }, function () {
+                    $state.reload();
+                });
+        };
+
         $scope.convidar = function (grupo) {
             //vai chamar um popup que cria um link para a pessoa se cadastrar já sendo adicionada
             //caso o email ja exista nos users ela somente é adicionada ao grupo.
@@ -76,14 +93,11 @@ angular.module('lunchtime').controller('GroupController', ['$scope', '$rootScope
                             return grupo
                         }
                     }
-                })
-                .result.then(function () {
-
+                }).result.then(function () {
+                    console.log("ok");
                 }, function () {
-
+                    console.log("cancel");
                 });
-
-
         };
 
         $scope.salvar = function (grupo) {
@@ -97,12 +111,12 @@ angular.module('lunchtime').controller('GroupController', ['$scope', '$rootScope
         $scope.tornarprincipal = function (grupo) {
             //tornar esse grupo o principal para este usuario.
             Usuario.getById($scope.iduser).then(function (usuario) {
-                if(usuario) {
+                if (usuario) {
                     usuario.grupoPrincipal = grupo._id.$oid;
-                    usuario.$saveOrUpdate().then(function (){
+                    usuario.$saveOrUpdate().then(function () {
                         $cookies.put("ltgrupoPrincipalKey", usuario.grupoPrincipal);
                         $cookies.putObject("ltgrupoPrincipal", grupo);
-                        $state.go("group",{user : $scope.iduser, grupo : usuario.grupoPrincipal});
+                        $state.go("group", {user: $scope.iduser, grupo: usuario.grupoPrincipal});
                     });
                 }
             });
@@ -123,7 +137,7 @@ angular.module('lunchtime').controller('GroupController', ['$scope', '$rootScope
                     $scope.grupos = grupos;
                     $scope.grupoPrincipal = usuario.grupoPrincipal;
 
-                    angular.forEach($scope.grupos, function(grupo){
+                    angular.forEach($scope.grupos, function (grupo) {
                         $scope.nomesOriginais.push(grupo.nome);
                     });
 
@@ -134,7 +148,7 @@ angular.module('lunchtime').controller('GroupController', ['$scope', '$rootScope
         };
 
         $scope.blurNome = function (index) {
-            if($scope.grupos[index].nome === "") {
+            if ($scope.grupos[index].nome === "") {
                 $scope.grupos[index].nome = $scope.nomesOriginais[index];
             }
         };
@@ -147,16 +161,77 @@ angular.module('lunchtime').controller('InviteController', function ($scope, ctr
 
     $scope.gerarLink = function () {
         if ($scope.emailconvite) {
-            var hashmd5 = md5.createHash(ctrlScope.iduser + "/" + grupo._id.$oid + "/" +  $scope.emailconvite);
+            var hashmd5 = md5.createHash(ctrlScope.iduser + "/" + grupo._id.$oid + "/" + $scope.emailconvite);
             $scope.linkhash = $location.protocol() + "://" + $location.host() + ":" + $location.port() + "/invite/" + hashmd5;
         }
     };
 
     $scope.confirmarEnvio = function () {
-        $scope.$close(true);
+        $scope.$close('ok');
     };
 
     $scope.cancelarEnvio = function () {
+        $scope.$dismiss('cancel');
+    };
+});
+
+angular.module('lunchtime').controller('AddGrupoController', function ($scope, ctrlScope, $modalInstance) {
+    $scope.newmapa = {};
+    var defaultposition = new google.maps.LatLng(-22.904265, -43.174494);
+    $scope.newgrupo = {
+        nome: "",
+        endereco: "",
+        latitude: "",
+        longitude: "",
+        criador: ctrlScope.iduser
+    };
+
+    angular.element(this).ready(function () {
+        var mapOptions = {
+            center: defaultposition,
+            zoom: 17,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            mapTypeControl: false,
+            streetViewControl: false,
+            disableDefaultUI: true
+        };
+        $scope.newmapa = new google.maps.Map(document.getElementById('map-canvas-new'), mapOptions);
+
+        var primarker = new MarkerWithLabel({
+            map: $scope.newmapa,
+            position: defaultposition,
+            labelContent: "",
+            labelClass: "marker-labels",
+            animation: google.maps.Animation.DROP,
+            labelAnchor: new google.maps.Point(($scope.newgrupo.nome.length * 3), 0)
+        });
+
+        var input = document.getElementById('endereco');
+        var searchBox = new google.maps.places.SearchBox(input);
+
+        google.maps.event.addListener(searchBox, 'places_changed', function () {
+            var places = searchBox.getPlaces();
+            var bounds = new google.maps.LatLngBounds();
+            var place = places[0];
+            primarker.setMap(null);
+            primarker.position = place.geometry.location;
+            primarker.setMap($scope.newmapa);
+            $scope.newmapa.setCenter(primarker.getPosition());
+
+            $scope.newgrupo.latitude = place.geometry.location.lat();
+            $scope.newgrupo.longitude = place.geometry.location.lng();
+
+        });
+    });
+
+    $scope.confirmarCriacao = function () {
+        if ($scope.newgrupo.nome && $scope.newgrupo.latitude && $scope.newgrupo.longitude) {
+            //tem algo preenchido
+            $scope.$close(true);
+        }
+    };
+
+    $scope.cancelarCriacao = function () {
         $scope.$dismiss();
     };
 });
